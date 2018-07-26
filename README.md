da7c449575cc380a0f85e76910a5667f4aae3c32
Caio Henrique Silva <caio.silva@kabum.com.br>
2018-07-26 18:08:20 -0300

    creating functions to get holidays and calculate days to add

---
+++ README.md
@@ -0,0 +1,2 @@
+# Py Working Days
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = '0.1.0'
+__author__ = "caio@caiowd.me"
+
+setuptools.setup(
+    name="py_workingdays",
+    version=__version__,
+    author=__author__,
+    author_email="caio@caiowd.me",
+    description="This is a python lib to deal with working days",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/caiohenriquesilva/py_workingdays",
+    packages=['py_workingdays'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
